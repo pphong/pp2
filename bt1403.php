d402712bf9e8284dcd7c9f7cb6ab2425c6e74b48
pphong <37360355+pphong@users.noreply.github.com>
2018-03-14 22:02:42 +0700

    Add files via upload

--- bt1403.php
+++ bt1403.php
@@ -18,12 +18,58 @@
             echo "</br>";
         }
         ?>
-        echo "</br>";
         
         
         <h4>Bai 2</h4>
         <form method="post" action="KQbt2.php">
             <input type="date" name="date">
+            <input type="submit" name="submit" value="submit">
+        </form>
+        </br>
+        
+        <h4>Bai 3</h4>
+        
+        <?php
+            $n = rand(10,100);$m = [];
+            echo "Xử lý mảng gồm $n phần tử như sau : </br> [";
+            $min=$max=0;
+            for ($i = 0 ; $i < $n ; $i++)
+            {
+                $m[$i] = rand(-100,100);
+                echo "$m[$i]   ";
+                
+                if ($m[$i]>$max) $max = $m[$i];
+                if ($m[$i]<$min) $min = $m[$i];
+            }
+            echo "    ]</br>";
+            $sum = 0;
+            foreach ($m as $pt){$sum += $pt;}
+            echo "</br>";
+            echo "Tong : $sum";
+            echo "</br>";
+            echo "Min :  ".$min;
+            echo "</br>";
+            echo "Max :  ".$max;
+            echo "</br>";
+            
+            echo "</br>";
+            echo "</br>";
+        ?>
+        
+        <h4>Bai 4</h4>
+        
+        <form method="post" action="res.php">
+            Số 1 : <input type="text" name="n1">;
+            Số 2 : <input type="text" name="n2">;
+            </br>
+            <input type="radio" name="cal" <?php if (isset($cal) && $cal=="min");?> value="-"> Trừ 
+            <input type="radio" name="cal" <?php if (isset($cal) && $cal=="plu");?> value="+"> Cộng
+            </br>
+            <input type="radio" name="cal" <?php if (isset($cal) && $cal=="mul");?> value="*"> Nhân
+            <input type="radio" name="cal" <?php if (isset($cal) && $cal=="div");?> value="/"> Chia
+            </br>
+            </br>
+
             <input type="submit" name="submit" value="submit">
         </form>
         
